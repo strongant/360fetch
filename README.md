6fe051b012289287b38437dbe36e6a7465a28b50
= <baiwh@spsp-it.com>
2016-04-26 11:12:29 +0800

    修改账户的存放方式,删除部分多余的代码

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 360fetch
-phantomjs  fetch  360
+`phantomjs  fetch  360`
+##注意
+###timeout时间可以自由调整
+###账户密码问题
+####在主目录文件下创建account.json,格式如:
+{
+  "account": "xxx",
+  "password": "xxx"
+}
+####建议将脚本作为定时任务进行每日抽奖
+####后期加入如果登陆时出现验证码择进行对验证码进行识别......
--- fetch.js
+++ fetch.js
@@ -1,5 +1,10 @@
-var page    = require("webpage").create();
-var fs      = require("fs");
+var page = require("webpage").create();
+var fs = require("fs");
+var logName = "fetch.log"
+var accounts = {
+  account: "18215361994",
+  password: "baiwenhui1994"
+};
 
 page.onError = function(msg, trace) {
 
@@ -8,7 +13,8 @@ page.onError = function(msg, trace) {
   if (trace && trace.length) {
     msgStack.push('TRACE:');
     trace.forEach(function(t) {
-      msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+      msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ?
+        ' (in function "' + t.function+'")' : ''));
     });
   }
 
@@ -16,127 +22,103 @@ page.onError = function(msg, trace) {
 
 };
 
-page.viewportSize = { width: 1920, height: 1080 };
+page.viewportSize = {
+  width: 1920,
+  height: 1080
+};
 
 var steps = [
-    // open the web page
-    function () {
-           // page.settings.userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36 QIHU 360EE';
-           // page.settings.Host = 'c16.yunpan.360.cn';
-           // page.settings.Connection = 'keep-alive';
-           // page.settings.Connection = 'keep-alive';
-           //page.settings.Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8';
-           page.open("http://c16.yunpan.360.cn/",function(status){
-                    // var cookies = page.cookies;
-                    // console.log('Listing cookies:');
-                    // for(var i in cookies) {
-                    //     console.log(cookies[i]+"\n");
-                    //     console.log(cookies[i].name + '=' + cookies[i].value);
+  // open the web page
+  function() {
+    page.settings.userAgent =
+      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36 QIHU 360EE';
+    page.open("http://c16.yunpan.360.cn/", function(status) {
+      if (status !== 'success') {
+        return
+      }
+      console.log(fs.read('account.json'));
+      fs.write(logName, "访问360云盘成功", 'a');
+    });
+  },
+  // fill the form and submit
+  function() {
 
-                    //   }
-           });
-    },
-    // fill the form and submit
-    function () {
-
-        // capture before submit form
-        page.render("first.jpg");
-        console.log('generate  first.jpg');
-        //page.switchToFrame("quc-form");
-        setTimeout(function(){
-            console.log('come');
-            page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', function() {
-                console.log('includejs  come');
-                page.evaluate(function () {
-                    console.log('evaluate  come');
-                    var $loginForm = $('form.quc-form');
-                    console.log('form:'+$loginForm.html());
-                    // var iframe = document.querySelector("#login_frame").contentDocument;
-                    $('input[name="account"]').val('18215361994');
-                    $('input[name="password"]').val('baiwenhui');
-                    //记住密码
-                    $('input[name=iskeepalive]').click();
-                    //測試賬號
-                    console.log('account:'+$('input[name="account"]').val());
-                    console.log('password:'+$('input[name="password"]').val());
-                    $('input[value="登录"]').click();
-                    //$loginForm.submit();
-                    // document.querySelector("#login_button").click();
-                    console.log("Login form is submitted");
-                    
-                });
-                //phantom.exit();
-            });
-        },5000);
-    },
-    function () {
-        // capture after submit form
-        setTimeout(function(){
-            page.render("second.jpg");
-            console.log('generate  second.jpg');
-        },5000);
-    },
-    function(){
-      setTimeout(function(){
-
-        page.evaluate(function(){
-            document.getElementById('lottery-everyday').click();
+    // capture before submit form
+    page.render("first.jpg");
+    console.log('generate  first.jpg');
+    fs.write(logName, "生成360云盘登陆页面图片", 'a');
+    //page.switchToFrame("quc-form");
+    setTimeout(function() {
+      page.includeJs(
+        'http://libs.baidu.com/jquery/2.0.0/jquery.js',
+        function() {
+          fs.write(logName, "请求百度jquery cdn文件成功", 'a');
+          page.evaluate(function(data) {
+            console.log('evaluate  come');
+            var $loginForm = $('form.quc-form');
+            console.log('form:' + $loginForm.html());
+            // var iframe = document.querySelector("#login_frame").contentDocument;
+            $('input[name="account"]').val(data['account']);
+            $('input[name="password"]').val(data['password']);
+            //记住密码
+            $('input[name=iskeepalive]').click();
+            //測試賬號
+            console.log('account:' + $('input[name="account"]').val());
+            console.log('password:' + $('input[name="password"]').val());
+            $('input[value="登录"]').click();
+            console.log("Login form is submitted");
+          }, accounts);
         });
-        //进行抽奖
-        // page.open('http://c16.yunpan.360.cn/my?p=signin',function(status){
-        //     console.log('choujiang:result:'+status);
-           
-        //     if('success'===status){
+    }, 5000);
+  },
+  function() {
+    // capture after submit form
+    setTimeout(function() {
+      page.render("second.jpg");
+      console.log('generate  second.jpg');
+      fs.write(logName, "进入用户首页页面并完成截图", 'a');
+    }, 5000);
+  },
+  function() {
+    setTimeout(function() {
 
-        //           //生成抽奖的图片
-        //         page.render("thrrid.jpg");
-        //         console.log('generate  thrrid.jpg');
+      page.evaluate(function() {
+        document.getElementById('lottery-everyday').click();
+      });
+    }, 8000);
 
-        //         //判断是否已经抽奖，如果抽过了，则不抽了
-        //         //var $baseDiv = document.getElementById('BasePanel1');
-        //         //console.log($baseDiv.innerHTML);
-        //         // if(undefined==$baseDiv||$baseDiv){
-
-        //         // }
-
-        //         //console.log('web plainText:'+page.content);
-        //     }else{
-        //         //需要验证码或者点击登录
-        //         console.log('choujiang failed');
-        //     }
-        //     phantom.exit();
-        // });
-      },5000);
-
-    },
-    function(){
-        setTimeout(function(){
-            //抽奖结果截图
-            page.render("result.jpg");
-            console.log('generate  result.jpg');
-        },3000);
-    }
+  },
+  function() {
+    setTimeout(function() {
+      //抽奖结果截图
+      page.render("result.jpg");
+      console.log('generate  result.jpg');
+      fs.write(logName, "抽奖完毕并完成截图", 'a');
+      phantom.exit();
+    }, 3000);
+  }
 ];
-var i = 0, loading = false;
+var i = 0,
+  loading = false;
 
 //测试5小时后抽奖
-var delay = 1000*60*10;
+var delay = 1000 * 60 * 10;
 
 
-setInterval(function () {
-    if (!loading && typeof steps[i] == "function") {
-        steps[i++]();
-    }
-}, 15000);
+setInterval(function() {
+  if (!loading && typeof steps[i] == "function") {
+    steps[i++]();
+  }
+}, 3000);
 
 page.onUrlChanged = function(targetUrl) {
   console.log('New URL: ' + targetUrl);
 };
 
 
-page.onLoadStarted = function () {
-    loading = true;
+page.onLoadStarted = function() {
+  loading = true;
 };
-page.onLoadFinished = function () {
-    loading = false;
+page.onLoadFinished = function() {
+  loading = false;
 };
